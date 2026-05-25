214bdc32816a59b6fee39056b1a049d017838fc4
Piyush1910 <piyushlondhe19@gmail.com>
2026-05-25 19:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def sensor_data():
         "temperature": random.randint(20, 40),
         "humidity": random.randint(40, 80)
     }
+
+    spray_status = "ON" if data["soil_moisture"] < 50 else "OFF"
+
+    data["spray_status"] = spray_status
+
     return jsonify(data)
 
 if __name__ == '__main__':
